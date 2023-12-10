203696556e77a58cfd499d21de46a168f5964ef2
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-10 23:54:33 +0600

    withdraw with input and total-

--- js/money.js
+++ js/money.js
@@ -1,3 +1,4 @@
+//deposit code
 //step 1: get depostit btn and
 document.getElementById("btn-deposit").addEventListener("click", function () {
   //step 2 input field get and string value convert to number (perseFloat)
@@ -21,5 +22,28 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const banalceSting = getBalance.innerText;
   const restBanalce = parseFloat(banalceSting);
   const totalBalance = deposit + restBanalce;
+  getBalance.innerText = totalBalance;
+});
+
+// withdraw
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+  const withdrawField = document.getElementById("withdraw-field");
+  const inputWithdrawString = withdrawField.value;
+  const inputWithdraw = parseFloat(inputWithdrawString);
+  //   console.log(typeof inputWithdraw, inputWithdraw);
+
+  const getTotalWithdraw = document.getElementById("total-withdraw");
+  const totalWithdrawString = getTotalWithdraw.innerText;
+  const restTotalWithdraw = parseFloat(totalWithdrawString);
+
+  const totalWithdraw = inputWithdraw + restTotalWithdraw;
+  getTotalWithdraw.innerText = totalWithdraw;
+  withdrawField.value = "";
+
+  //   main balace
+  const getBalance = document.getElementById("balance");
+  const banalceSting = getBalance.innerText;
+  const restBanalce = parseFloat(banalceSting);
+  const totalBalance = restBanalce - inputWithdraw;
   getBalance.innerText = totalBalance;
 });
